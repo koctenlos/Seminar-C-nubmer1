6cb81e9a0ca8dfc3db3b8cae502801b102c5640c
PC-1 <danilnub01@gmail.com>
2022-09-18 19:49:59 +0700

    add Max_Iz_3h

---
+++ Max_Iz_3h/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Введите число 2: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Введите число 3: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+
+if (b > a)
+    max = b;
+if (c > max)
+    max = c;
+
+Console.WriteLine($"Максимальное число - {max}");
