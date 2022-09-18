7262fa86ff1122828d253232416c8c33c3305076
PC-1 <danilnub01@gmail.com>
2022-09-18 18:14:53 +0700

    add Nazvanie_Dnya_Po_Nomeru

---
+++ Nazvanie_Dnya_Po_Nomeru/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.Clear();
+Console.Write("Введите число от 1 до 7: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if(num==1){
+    Console.Write("Понедельник");
+}
+if(num==2){
+    Console.Write("Вторник");
+}
+if(num==3){
+    Console.Write("Среда");
+}
+if(num==4){
+    Console.Write("Четверг");
+}
+if(num==5){
+    Console.Write("Пятница");
+}
+if(num==6){
+    Console.Write("Суббота");
+}
+if(num==7){
+    Console.Write("Воскресенье");
+}
