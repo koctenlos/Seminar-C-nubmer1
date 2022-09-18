2416c4ced58f0efab40d2980a8adb8c211c84cc8
PC-1 <danilnub01@gmail.com>
2022-09-18 19:57:41 +0700

    add Chetnoe_Li

---
+++ Chetnoe_Li/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число : ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if((a%2)==0){
+    Console.WriteLine("Да");
+}
+else{
+    Console.WriteLine("Нет");
+}
