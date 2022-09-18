088b422a556f67a2c451025b9635c1c22efc7ea7
PC-1 <danilnub01@gmail.com>
2022-09-18 18:44:20 +0700

    add Chisla_ot_MinusA_do_PlusA

---
+++ Chisla_ot_MinusA_do_PlusA/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+
+int count=0;
+int max=0;
+
+if(N<0){
+    count = N;
+    max= -N;
+}
+else
+{
+    count = -N;
+    max = N;
+}
+
+while(count <= N)
+{
+    Console.WriteLine(count);
+    count += 1;
+}
