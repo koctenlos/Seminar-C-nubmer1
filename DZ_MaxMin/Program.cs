f29905dbe65c79aaeb4762161beb71f319f5e768
PC-1 <danilnub01@gmail.com>
2022-09-18 19:24:53 +0700

    add DZ_MaxMin

---
+++ DZ_MaxMin/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число 1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.WriteLine("Введите число 2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if(num1>num2){
+    Console.WriteLine($"Максимальное число - {num1}");
+}
+else{
+    Console.WriteLine($"Максимальное число - {num2}");
+}
+if(num1<num2){
+    Console.WriteLine($"Миниимальное число - {num1}");
+}
+else{
+    Console.WriteLine($"Минимальное число - {num2}");
+}
